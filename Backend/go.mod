abf35ddbc6a1ade320e5ea33c0a4d45b7d9fdfb8
Conor Callanan <conorcallanan03@gmail.com>
2023-01-03 21:18:30 +0000

    Adding backend

---
+++ Backend/go.mod
@@ -0,0 +1,3 @@
+module example/soccer_scores
+
+go 1.19
---
+++ Backend/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Invalid Request\n")
+}
+func getLeagues(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w)
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/leagues", getLeagues)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+
+	apiCaller()
+
+}
+func apiCaller() (leagues map[string][]Team) {
+
+	resp, err := http.Get("https://api.soccersapi.com/v2.2/leagues/?user=conor.callanan1&token=54fa83edd3897eccee4f88463a3e6a9e&t=list")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var response Response
+	json.Unmarshal([]byte(body), &response)
+
+	for i := 0; i < len(response.Data); i++ {
+
+		var league = response.Data[i]
+
+		fmt.Printf("\n\nLeague Name: %s, League ID: %s, League Country: %s, Current Season ID: %s Requests Remaining: %d \n\n", league.Name, league.Id, league.Country_Name, league.Current_Season_Id, response.Meta.Requests_Left)
+
+		resp, err := http.Get("https://api.soccersapi.com/v2.2/leagues/?user=conor.callanan1&token=54fa83edd3897eccee4f88463a3e6a9e&t=standings&season_id=" + league.Current_Season_Id)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		var response StandingsOuterResponse
+		json.Unmarshal([]byte(body), &response)
+
+		var teams = response.Data.Standings
+
+		for i := 0; i < len(teams); i++ {
+			fmt.Printf("Team Name: %s, Team Position: %d \n", teams[i].Team_Name, teams[i].Overall.Position)
+		}
+
+	}
+
+}
+
+type Response struct {
+	Data []League
+	Meta Meta
+}
+
+type StandingsOuterResponse struct {
+	Data StandingsResponse
+	Meta Meta
+}
+
+type StandingsResponse struct {
+	Standings []Team
+}
+
+type League struct {
+	Id                string
+	Current_Season_Id string
+	Name              string
+	Country_Name      string
+}
+
+type Team struct {
+	Team_Name string
+	Overall   TeamStats
+}
+
+type TeamStats struct {
+	Position     int
+	Won          int
+	Draw         int
+	Lost         int
+	Points       int
+	Games_Played int
+}
+
+type Meta struct {
+	Requests_Left int
+	Pages         int
+	Count         int
+}
